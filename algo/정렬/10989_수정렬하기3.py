ab6abf73f3fa16ff4e957b5c71ccad6194f96171
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-04-17 19:03:02 +0900

    [정렬] 10989_수정렬하기3.py

--- algo/정렬/10989_수정렬하기3.py
+++ algo/정렬/10989_수정렬하기3.py
@@ -3,13 +3,9 @@ input = sys.stdin.readline
 
 arr=[0]*10001
 n=int(input())
-
 for i in range(n):
-    arr[int(input())]+=1
-
-
-for i in range(0,10001):
-    if arr[i]!=0:
-        for j in range(arr[i]):
-            print(str(i))
-
+    arr[int(input())]+=1;
+for i in range(len(arr)):
+    if(arr[i]!=0):
+        for k in range(arr[i]):
+            print(i)
