3f10689fe070df38695a3452810a621602d23da1
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-04-17 19:42:32 +0900

    [정렬] 1931_회의실배정.py

--- algo/정렬/1931_회의실배정.py
+++ algo/정렬/1931_회의실배정.py
@@ -1,18 +1,30 @@
 import sys
 input = sys.stdin.readline
-n= int(input())
-end_time=0;
-cnt=0;
+# n= int(input())
+# end_time=0;
+# cnt=0;
 
-Zoom = [list(map(int,input().split())) for i in range(n)]
-Zoom.sort(key=lambda x:(x[1],x[0]))
+# Zoom = [list(map(int,input().split())) for i in range(n)]
+# Zoom.sort(key=lambda x:(x[1],x[0]))
 
 
-for i in Zoom:
-    if(i[0]>=end_time):
+# for i in Zoom:
+#     if(i[0]>=end_time):
+#         cnt+=1
+#         end_time=i[1]
+
+# print(cnt)
+
+all_time=[]
+cnt=0
+end_time=0
+n=int(input())
+for i in range(n):
+    all_time.append(list(map(int,input().split())))
+
+all_time.sort(key=lambda x: (x[1],x[0]))
+for i in all_time:
+    if(end_time<=i[0]):
         cnt+=1
         end_time=i[1]
-
 print(cnt)
-
-
