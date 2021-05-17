f06db5aca0cd440e91a4b442dd5a34948e89d402
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-05-18 01:24:38 +0900

    [JS] 프로그래머스 신규아이디추천(오답)

--- algo/JS/Programmers/recommentID.js
+++ algo/JS/Programmers/recommentID.js
@@ -4,17 +4,37 @@ function solution(new_id) {
   new_id = new_id.toLowerCase();
 
   const checkEffectWord = (str) => {
-    const effectWord = /[^(/-\/_\/.\a-z0-9)]/gi;
+    const effectWord = /[^-_.a-z0-9)]/gi;
     if (effectWord.test(str)) {
       var newStr = str.replace(effectWord, '');
       return newStr;
     } else return str;
   };
 
-  const checkPoint = () => {
-    var newStr = str.replace('..', '.');
+  const checkDoublePoint = (str) => {
+    var newStr = str.replace(/[.]{2,}/g, '.');
     return newStr;
   };
-}
 
-var temp = checkEffectWord(new_id);
+  const checkStartPoint = (str) => {
+    var newStr = str.replace(/(^[.])|([.]$)/g, '');
+    return newStr;
+  };
+
+  var temp = checkStartPoint(checkDoublePoint(checkEffectWord(new_id)));
+  if (!temp) temp = 'a';
+  if (temp.length >= 16) temp = temp.slice(0, 15);
+  console.log('점전', temp);
+
+  if (temp.substr(temp.length - 1, 1) === '.')
+    temp = temp.substr(0, temp.length - 1);
+  console.log('점후', temp);
+  if (temp.length <= 2) {
+    var lastWord = temp.substr(temp.length - 1, 1);
+    while (temp.length < 3) {
+      temp += lastWord;
+    }
+  }
+
+  return temp;
+}
