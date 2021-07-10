927e53f76950c437645b6cef4f0d1ef5aba51f77
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-07-11 03:14:52 +0900

    [BJ] 1920_수찾기

--- algo/Python/hackerRank/binarySearch.py
+++ algo/Python/hackerRank/binarySearch.py
@@ -1,6 +1,7 @@
 # bisect_right : start
 # bisect_left : end
 # 존재하는 값만 찾을 때 : end 
+arr = [1,2,4,5,7]
 def bin_search(target,data) :
     start=0
     end=len(data)-1
@@ -17,14 +18,24 @@ def bin_search(target,data) :
     return end
 
 
-data=[1,3,5,9]
-target1=1
-target2=2
-target3=3
-target4=4
+# data=[1,3,5,9]
+# target1=1
+# target2=2
+# target3=3
+# target4=4
 
 
-print('같은거: 1-----',bin_search(target1,data))
-print('다른거: 2-----',bin_search(target2,data))
-print('같은거: 3-----',bin_search(target3,data))
-print('다른거: 4-----',bin_search(target4,data))
+# print('같은거: 1-----',bin_search(target1,data))
+# print('다른거: 2-----',bin_search(target2,data))
+# print('같은거: 3-----',bin_search(target3,data))
+# print('다른거: 4-----',bin_search(target4,data))
+
+# arr = [1,2,4,5,7] 
+
+from bisect  import bisect_right 
+print('라이브러리',bisect_right(arr,4))
+print('라이브러리',bisect_right(arr,3))
+
+
+print(bin_search(4,arr));
+print(bin_search(3,arr));
