a88eec258a271875641e362e0ef9e7e5a6ca059f
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-07-30 17:31:06 +0900

    [iC] 다이나믹프로그래밍 개미전사

---
+++ algo/Python/isCoding/dynamic/antWar.py
@@ -0,0 +1,12 @@
+def solution():
+    n = int(input())
+    food = list(map(int,input().split()))
+
+    d= [0]*100
+    d[0] = food[0]
+    d[1] = max(food[0],food[1])
+    for i in range(2,n):
+        d[i] = max(d[i-1],d[i-2]+food[i])
+    return d[n-1]
+
+print(solution())
