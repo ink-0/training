215f57e84b4d33b64e20a08472b5d40a17a371e8
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-07-30 15:41:08 +0900

    [iC] 다이나믹프로그래밍 피보나치수열

---
+++ algo/Python/isCoding/dynamic/fibo.py
@@ -0,0 +1,18 @@
+d = [0]*100
+## 재귀 방식
+# def fibo(x):
+#     if x==1 or x==2:
+#         return 1
+#     if d[x]!= 0:
+#         return d[x]
+#     d[x] = fibo(x-1)+fibo(x-2)
+#     return d[x]
+
+## 반복문 방식
+def fibo(x):
+    d[1]=1
+    d[2]=1
+    for i in range(3,x+1):
+        d[i] = d[i-1]+d[i-2]
+    return d[x]
+print(fibo(6))
