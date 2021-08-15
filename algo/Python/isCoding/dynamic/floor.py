e1cdf4afa1762c81b862c2bfcb0a636c59f989d0
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-08-15 23:56:04 +0900

    [iC] 다이나믹프로그래밍 바닥공사

--- algo/Python/isCoding/dynamic/floor.py
+++ algo/Python/isCoding/dynamic/floor.py
@@ -1,9 +1,10 @@
 n = int(input())
+
 d = [0]*1001
-
-d[1]=1
+d[1] = 1
 d[2]=3
-
 for i in range(3,n+1):
-    d[i] = (d[i-1]+2*d[i-2])%796796
+    d[i] = (d[i-1]+d[i-2]*2)%796796
+    print(d[i])
+
 print(d[n])
