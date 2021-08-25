17dc9be065816025c5b4e15de30f0e4b9c78c5f1
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-08-26 01:54:53 +0900

    [iC] 그리디_만들수 없는 금액 수정

--- algo/Python/isCoding/greedy/previous/cantBill.py
+++ algo/Python/isCoding/greedy/previous/cantBill.py
@@ -1,19 +1,12 @@
 n = int(input())
 money  = list(map(int,input().split()))
 
-# maxM = max(money)
-
-# confirm = [0 for i in range(10000)]
-# print(confirm)
-# for i in range(len(money)):
-#     for j in range(i+1,len(money)):
-#         confirm[i+j] = 1
-
-# print(confirm.find(0,1))
+money.sort()
+target = 1
 
 for m in money:
     if target<m:
         break
     target += m
 
-pirnt(target)
+print(target)
