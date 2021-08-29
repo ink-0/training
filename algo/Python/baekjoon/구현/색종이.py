2f04da25b01f33d6882782827c7c627d37391049
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-08-29 15:32:18 +0900

    [BJ] 구현_색종이수정

--- algo/Python/baekjoon/구현/색종이.py
+++ algo/Python/baekjoon/구현/색종이.py
@@ -5,21 +5,19 @@ for i in range(n):
     paper.append(list(map(int,input().split())))
 
 
+place = [[0]*10 for _ in range(10)]
 for i,p in enumerate(paper):
     cnt=0
-    place = [[0]*1001 for _ in range(1001)]
     [row,col,area,height] = p
+    # for r in range(row,row+area):
+    #     for c in range(col,col+height):
+    #         place[r][c]=i+1
+
     for r in range(row,row+area):
-        for c in range(col,col+height):
-            place[r][c]=i+1
+        place[r][col:col+height] = [i+1]*height
 
 for i in range(n):
     cnt=0
     for p in place:
         cnt += p.count(i+1)
     print(cnt)
-
-
-
-print('답',cnt)
-    
