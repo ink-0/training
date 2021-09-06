5a1e1c06faf7d06dec3c7b46bcf6e14471b90b14
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-09-06 23:51:04 +0900

    [iC] 구현 문자열 압축

--- algo/Python/isCoding/implementation/previous/pressureString.py
+++ algo/Python/isCoding/implementation/previous/pressureString.py
@@ -11,3 +11,38 @@ def solution(s):
             if prev == s[j:j+step]:
                 cnt+1
         
+
+def solution(s):
+    answer = len(s)
+
+    for i in range(1, len(s) // 2 + 1):
+
+        cnt = 1
+        ghStr = ''
+        sliceArr = list(map(''.join, zip(*[iter(s)] * i)))
+        prev = ''
+
+        for word in sliceArr:
+            if prev == word:
+                cnt += 1
+            else:
+                if cnt == 1:
+                    ghStr += prev
+                else:
+                    ghStr += str(cnt) + prev
+
+                prev = word
+                cnt = 1
+
+        if cnt == 1:
+            ghStr += prev
+        else:
+            ghStr += str(cnt) + prev
+
+        temp = "".join(sliceArr)
+
+        if len(s) != len(temp):
+            ghStr += s[len(temp):]
+        answer = min(len(ghStr), answer)
+
+    return answer
