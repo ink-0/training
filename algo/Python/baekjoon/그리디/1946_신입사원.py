e16d9e9e902c7efd4f49d844e1ccf02731b5bf5c
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-09-13 23:59:00 +0900

    [BJ] 신입사원

--- algo/Python/baekjoon/그리디/1946_신입사원.py
+++ algo/Python/baekjoon/그리디/1946_신입사원.py
@@ -12,7 +12,7 @@ for i in range(0,T):
         people.append([Paper, Interview])
 
     people.sort() # 서류 기준 오름차순 정렬
-    Max = people[0][1]
+    Max = people[0][2]
     
     for i in range(1,N):
         if Max > people[i][1]:
