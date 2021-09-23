4abf9ed38f032f9ff63064c7eaaea5cfcec162ed
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-09-24 00:04:45 +0900

    [CD] 4-2_MaxCounter2

--- algo/JS/codility/4-3_MaxCounter.js
+++ algo/JS/codility/4-3_MaxCounter.js
@@ -1,24 +1,26 @@
+// function solution(N, A) {
+//   let ans = new Array(N).fill(0);
+//   A.forEach((a) => {
+//     if (a <= N) {
+//       ans[a - 1] += 1;
+//     }
+//   });
+
+//   return ans;
+// }
+
 function solution(N, A) {
+  // write your code in JavaScript (Node.js 8.9.4)
   let ans = new Array(N).fill(0);
+  let maxNum = 0;
+
   A.forEach((a) => {
+    let idx = a - 1;
     if (a <= N) {
-      ans[a - 1] += 1;
-    }
+      ans[idx] += 1;
+      maxNum = Math.max(maxNum, ans[idx]);
+    } else ans.fill(maxNum);
   });
 
   return ans;
-}
-function solution(N, A) {
-  const counter = new Array(N).fill(0);
-  let maxCounter = 0;
-
-  A.forEach((item) => {
-    let idx = item - 1;
-    if (item <= N) {
-      counter[idx] += 1;
-      maxCounter = Math.max(maxCounter, counter[idx]);
-    } else counter.fill(maxCounter);
-  });
-
-  return counter;
 }
