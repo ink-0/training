7a1baefab38149d0682bff640abd77eb1d53d08f
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-09-28 00:32:58 +0900

    [BJ]_4949_BalancedWorld2

--- algo/JS/baekjoon/4949_balancedWorld.js
+++ algo/JS/baekjoon/4949_balancedWorld.js
@@ -5,17 +5,22 @@ let input = (fs.readFileSync('./test') + '').toString().trim().split('.');
 input.pop();
 input.pop();
 
-const bracketStack = [];
-
 // 각 괄호가 나올때마다 조건ㅁ문 하기
 const ans = input.map((sentence) => {
-  sentence.forEach((alpha) => {
+  const bracketStack = [];
+  sentence.split('').forEach((word) => {
     if (word === '(' || word === '[') {
       bracketStack.push(word);
     } else if (word === ')') {
-      if (bracketStack.pop() !== '(') return 'no';
+      if (bracketStack[bracketStack.length - 1] !== '(') return 'no';
+      else bracketStack.pop();
     } else if (word === ']') {
-      if (bracketStack.pop() !== '[') return 'no';
+      if (bracketStack[bracketStack.length - 1] !== '[') return 'no';
+      else bracketStack.pop();
     }
   });
+
+  if (bracketStack.length !== 0) return 'no';
+  return 'yes';
 });
+ans.forEach((a) => console.log(a));
