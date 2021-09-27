51f6de69ab2ff8c1423ea48bfc8331810e2baa5e
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-09-27 23:51:43 +0900

    [BJ]_4949_BalancedWorld

--- algo/JS/baekjoon/4949_balancedWorld.js
+++ algo/JS/baekjoon/4949_balancedWorld.js
@@ -5,21 +5,17 @@ let input = (fs.readFileSync('./test') + '').toString().trim().split('.');
 input.pop();
 input.pop();
 
-console.log(input);
+const bracketStack = [];
 
-const regL = /\[/g;
-const regR = /\[/g;
-const regl = /\(/g;
-const regr = /\)/g;
-
-const ans = input.map((i) => {
-  const regLCnt = i.match(regL) ? i.match(regL).length : 0;
-  const regRCnt = i.match(regR) ? i.match(regR).length : 0;
-  const reglCnt = i.match(regl) ? i.match(regl).length : 0;
-  const regrCnt = i.match(regr) ? i.match(regr).length : 0;
-  console.log(i, 'i', regLCnt, regRCnt, reglCnt, regrCnt);
-  if (regLCnt === regRCnt && reglCnt === regrCnt) return 'yes';
-  else return 'no';
+// 각 괄호가 나올때마다 조건ㅁ문 하기
+const ans = input.map((sentence) => {
+  sentence.forEach((alpha) => {
+    if (word === '(' || word === '[') {
+      bracketStack.push(word);
+    } else if (word === ')') {
+      if (bracketStack.pop() !== '(') return 'no';
+    } else if (word === ']') {
+      if (bracketStack.pop() !== '[') return 'no';
+    }
+  });
 });
-
-console.log(ans);
