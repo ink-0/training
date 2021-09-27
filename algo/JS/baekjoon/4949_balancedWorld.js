6f8f1bee649e95126c5ac5292e51e8366cd8a7d4
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-09-28 00:52:53 +0900

    [BJ]_4949_BalancedWorld3

--- algo/JS/baekjoon/4949_balancedWorld.js
+++ algo/JS/baekjoon/4949_balancedWorld.js
@@ -3,26 +3,47 @@ const fs = require('fs');
 let input = (fs.readFileSync('./test') + '').toString().trim().split('\n');
 const open = ['(', '['];
 const close = [')', ']'];
-
-const bool = [];
+let bracketStack;
+const ans = [];
 // 각 괄호가 나올때마다 조건ㅁ문 하기
-console.log('인풋', input);
-console.log('배열', input.slice(0, input.length - 1));
-const ans = input.map((sentence) => {
-  const bracketStack = [];
-  sentence.split('').forEach((word) => {
-    if (word === '(' || word === '[') {
-      bracketStack.push(word);
-    } else if (word === ')') {
-      if (bracketStack[bracketStack.length - 1] !== '(') return 'no';
-      else bracketStack.pop();
-    } else if (word === ']') {
-      if (bracketStack[bracketStack.length - 1] !== '[') return 'no';
-      else bracketStack.pop();
-    }
-  });
+input.pop();
+input.forEach((sentence) => {
+  let isBool = false;
+  bracketStack = [];
 
-  if (bracketStack.length !== 0) return 'no';
-  return 'yes';
+  for (let i = 0; i < sentence.length; i++) {
+    if (open.includes(sentence[i])) bracketStack.push(sentence[i]);
+    else if (close.includes(sentence[i])) {
+      if (bracketStack.pop() !== open[close.indexOf(sentence[i])]) {
+        ans.push('no');
+        isBool = true;
+        break;
+      }
+    }
+  }
+  if (!isBool) {
+    if (bracketStack.length === 0) ans.push('yes');
+    else ans.push('no');
+  }
 });
-ans.forEach((a) => console.log(a));
+
+console.log(ans.join('\n'));
+
+// const ans = input.map((sentence) => {
+//   const bracketStack = [];
+//   sentence.split('').forEach((word) => {
+//     if (word === '(' || word === '[') {
+//       bracketStack.push(word);
+//     } else if (word === ')') {
+//       if (bracketStack[bracketStack.length - 1] !== '(') return 'no';
+//       else bracketStack.pop();
+//     } else if (word === ']') {
+//       if (bracketStack[bracketStack.length - 1] !== '[') return 'no';
+//       else bracketStack.pop();
+//     }
+//   });
+
+//   if (bracketStack.length !== 0) return 'no';
+//   return 'yes';
+// });
+// ans.forEach((a) => console.log(a));
