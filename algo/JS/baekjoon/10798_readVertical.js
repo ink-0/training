25885c6d51aebf73491c370e4d9b11cc85903180
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-09-29 02:33:46 +0900

    [BJ] 10798_readVertical2

--- algo/JS/baekjoon/10798_readVertical.js
+++ algo/JS/baekjoon/10798_readVertical.js
@@ -1,8 +1,17 @@
 const fs = require('fs');
 // const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 // const input = fs.readFileSync("./test").toString().trim().split("\n");
-let input = (fs.readFileSync('./test') + '').trim().split('\n');
 
-input.shift();
+let input = (fs.readFileSync('./test') + '').toString().trim().split('\n');
 
-console.log(input);
+const turnCnt = Math.max(...input.map((i) => i.length));
+
+let ans = '';
+
+for (let i = 0; i < turnCnt; i++) {
+  for (let j = 0; j < input.length; j++) {
+    ans += input[j][i] || '';
+  }
+}
+
+console.log(ans);
