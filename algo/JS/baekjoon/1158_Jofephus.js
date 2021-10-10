d1d1039da3689f10a9e10304c60b6599f3522e18
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-10-11 02:44:40 +0900

    [BJ] 1158_요세푸스문제3

--- algo/JS/baekjoon/1158_Jofephus.js
+++ algo/JS/baekjoon/1158_Jofephus.js
@@ -5,15 +5,25 @@ let [n, k] = input.map((n) => parseInt(n));
 let ansArr = [];
 const arr = Array.from({ length: n }, (v, i) => i + 1);
 
-for (let i = 0; i < n; i++) {
-  for (let j = 1; j <= k; j++) {
-    if (j === k) {
-      ansArr.push(arr.shift());
-    } else {
-      arr.push(arr.shift());
-    }
-  }
-}
+// for (let i = 0; i < n; i++) {
+//   for (let j = 1; j <= k; j++) {
+//     if (j === k) {
+//       ansArr.push(arr.shift());
+//     } else {
+//       arr.push(arr.shift());
+//     }
+//   }
+// }
 
+// const ans = '<' + ansArr.join(', ') + '>';
+// console.log(ans);
+
+let cnt = 0;
+
+while (arr.length > 0) {
+  cnt = (cnt + k - 1) % arr.length;
+  ansArr.push(arr[cnt]);
+  arr.splice(cnt, 1);
+}
 const ans = '<' + ansArr.join(', ') + '>';
 console.log(ans);
