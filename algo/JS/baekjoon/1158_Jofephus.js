63e9106b91f8ba18a9a13b5410ad7b77d4749f6e
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-10-11 02:20:28 +0900

    [BJ] 1158_요세푸스문제2

--- algo/JS/baekjoon/1158_Jofephus.js
+++ algo/JS/baekjoon/1158_Jofephus.js
@@ -1,23 +1,19 @@
 const fs = require('fs');
 
 let input = (fs.readFileSync('./test') + '').toString().trim().split(' ');
+let [n, k] = input.map((n) => parseInt(n));
+let ansArr = [];
+const arr = Array.from({ length: n }, (v, i) => i + 1);
 
-let n = parseInt(input[0]);
-let k = parseInt(input[1]);
-let arr = [];
-let answer = '';
-let deleted = [];
 for (let i = 0; i < n; i++) {
-  arr[i] = i + 1;
-}
-for (j = 0; j < n; j++) {
-  for (let m = 1; m <= k; m++) {
-    if (m === k) {
-      deleted.push(arr.shift());
+  for (let j = 1; j <= k; j++) {
+    if (j === k) {
+      ansArr.push(arr.shift());
     } else {
       arr.push(arr.shift());
     }
   }
 }
-answer = '<' + deleted.join(', ') + '>';
-console.log(answer);
+
+const ans = '<' + ansArr.join(', ') + '>';
+console.log(ans);
