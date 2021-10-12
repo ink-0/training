42357e43a51b662e8a0e1e23e676f98c3158a2e5
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-10-12 23:43:42 +0900

    [BJ]JS 2164_카드2_2

--- algo/JS/baekjoon/2164_card2.js
+++ algo/JS/baekjoon/2164_card2.js
@@ -2,11 +2,69 @@ const fs = require('fs');
 
 let input = (fs.readFileSync('./test') + '').toString().trim();
 
-const numArr = Array.from({ length: parseInt(input) }, (v, i) => i + 1);
+// while (numArr.length > 1) {
+//   numArr.shift();
+//   numArr.push(numArr.shift());
+// }
 
-while (numArr.length > 1) {
-  numArr.shift();
-  numArr.push(numArr.shift());
+// console.log(numArr[0]);
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
 }
 
-console.log(numArr[0]);
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this._size = 0;
+  }
+
+  add(value) {
+    const newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+    }
+
+    this.tail = newNode;
+    this._size++;
+
+    return newNode;
+  }
+
+  getHead() {
+    return this.head.value;
+  }
+
+  removeHead() {
+    this.head = this.head.next;
+    this.head.prev = null;
+    this._size--;
+  }
+
+  getSize() {
+    return this._size;
+  }
+}
+
+function getLastCard(n) {
+  const cards = new LinkedList();
+
+  for (let i = 1; i <= n; i++) {
+    cards.add(i);
+  }
+  while (cards.getSize() !== 1) {
+    cards.removeHead();
+    cards.add(cards.getHead());
+    cards.removeHead();
+  }
+
+  return cards.getHead();
+}
