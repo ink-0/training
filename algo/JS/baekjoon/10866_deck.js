5cd2ad2dccb802f1ff0fdd9c68a686a9dd9969d6
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-10-17 02:24:19 +0900

    [BJ] 10866_덱2

--- algo/JS/baekjoon/10866_deck.js
+++ algo/JS/baekjoon/10866_deck.js
@@ -6,23 +6,32 @@ input.shift();
 
 let queue = [];
 
-console.log(input);
-
 const cmdObj = {
-  push_back: (x) => queue.push(),
-  push_front: (x) => queue.unshift(),
+  push_back: (x) => {
+    queue.push(x);
+    return '';
+  },
+  push_front: (x) => {
+    queue.unshift(x);
+    return '';
+  },
   pop_front: () => queue.shift() || -1,
   pop_back: () => queue.pop() || -1,
-  size: () => queue.length(),
+  size: () => queue.length,
   empty: () => (queue[0] ? 0 : 1),
-  front: () => queue[0],
-  back: () => queue[queue.length - 1],
+  front: () => queue[0] || -1,
+  back: () => queue[queue.length - 1] || -1,
 };
 
-const result = input.reduce((acc, v) => {
-  if (cmdObj[v]) return `${cmdObj[v]()}\n`;
-  else {
-  }
-});
+const result = input.reduce(
+  (acc, v) =>
+    acc +
+    (cmdObj[v]
+      ? `${cmdObj[v]()}\n`
+      : v.split(' ')[0] === 'push_back'
+      ? cmdObj.push_back(v.split(' ')[1])
+      : cmdObj.push_front(v.split(' ')[1])),
+  ''
+);
 
 console.log(result);
