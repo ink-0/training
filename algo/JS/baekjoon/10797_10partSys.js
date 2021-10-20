4d346524aa42452cc323662ee1f3ed3b78d28704
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-10-21 02:04:45 +0900

    [BJ] JS 10797_10부제2

--- algo/JS/baekjoon/10797_10partSys.js
+++ algo/JS/baekjoon/10797_10partSys.js
@@ -1,20 +1,9 @@
 const fs = require('fs');
 
-let input = (fs.readFileSync('./test') + '').toString().trim().split('\n');
+let [day, carNumArr] = (fs.readFileSync('./test') + '')
+  .toString()
+  .trim()
+  .split('\n');
 
-let day = parseInt(input.shift());
-let carNumArr = input[0].split(' ').map((ele) => parseInt(ele));
-
-let cnt = 0;
-
-carNumArr.map((carNum) => {
-  let banDays = [];
-  if (carNum !== 0) {
-    banDays = [carNum, carNum + 10, carNum + 20];
-  } else {
-    banDays = [10, 20, 30];
-  }
-
-  if (banDays.indexOf(day) >= 0) cnt += 1;
-});
-console.log(cnt);
+const ans = carNumArr.split(' ').filter((num) => num === day).length;
+console.log(ans);
