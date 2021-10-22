448f7e4195c14e23d7856146681aed2284e9bca2
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-10-23 03:06:08 +0900

    [BJ] 1448_삼각형만들기2

--- algo/JS/baekjoon/1448_makejTriangle.js
+++ algo/JS/baekjoon/1448_makejTriangle.js
@@ -1,24 +1,20 @@
 const fs = require('fs');
 
-let input = (fs.readFileSync('./test') + '').toString().trim().split('\n');
-
+let input = (fs.readFileSync('./dev/stdin') + '').toString().trim().split('\n');
 const n = +input.shift();
-let sumArr = [];
-input = input.map((line) => parseInt(line));
-input.sort((a, b) => parseInt(b) - parseInt(a));
-// console.log(n, strawArr);
-
+input = input.map((a) => parseInt(a)).sort((a, b) => b - a);
+let ans = 0;
 const checkTri = (a, b, c) => {
   return a < b + c;
 };
 
 for (let i = 0; i < n - 2; i++) {
-  //   console.log('i', i);
   if (checkTri(input[i], input[i + 1], input[i + 2])) {
     ans = [input[i], input[i + 1], input[i + 2]].reduce(
       (acc, cur) => acc + cur
     );
-    sumArr.push(ans);
+    break;
   }
 }
-console.log(Math.max(...sumArr));
+
+console.log(ans === 0 ? -1 : ans);
