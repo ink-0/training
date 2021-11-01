6f5351e2f9ad8872617aed539984562586b5c4da
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-11-02 02:01:55 +0900

    [BJ] JS 2606_바이러스3:

--- algo/JS/baekjoon/2606_virus.js
+++ algo/JS/baekjoon/2606_virus.js
@@ -1,30 +1,33 @@
 const fs = require('fs');
 
 let input = (fs.readFileSync('./test') + '').toString().trim().split('\n');
-
-const computerCnt = Number(input.shift());
-const pair = Number(input.shift());
-let graph = [...Array(computerCnt + 1)].map(() => []);
-let visited = Array(computerCnt + 1).fill(false);
+let n = Number(input.shift());
+let m = Number(input.shift());
+let graph = [...new Array(n + 1)].map(() => []);
+let visited = new Array(n + 1).fill(false);
 let ans = 0;
+console.log('그래프', graph);
+visited[1] = true;
 
-const dfs = (v) => {
-  if (visited[v]) {
-    return;
-  }
-  visited[v] = true;
-  ans++;
-  graph[v].forEach((vertex) => {
-    if (!visited[vertex]) {
-      dfs(vertex);
+const dfs = (start) => {
+  for (const dest of graph[start]) {
+    console.log('데스트', dest);
+    if (!visited[dest]) {
+      console.log('&&&&&', visited[dest]);
+      visited[dest] = true;
+      ans += 1;
+      dfs(dest);
     }
-  });
+  }
+  console.log('***********', visited);
+  console.log('-----------', graph);
 };
 
-for (let i = 0; i < computerCnt - 1; i++) {
-  let [num1, num2] = input[i].split(' ').map((num) => Number(num));
-  graph.push(num1);
-  graph.push(num2);
-  dfs(1);
-}
-console.log(ans - 1);
+input.map((i) => {
+  const [start, dest] = i.split(' ').map((ele) => Number(ele));
+  graph[start].push(dest);
+  graph[dest].push(start);
+});
+
+dfs(1);
+console.log(ans);
