5a6f21509740047225c18193ff271f647fbfcf9e
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-11-04 23:59:06 +0900

    [BJ] JS 2606바이러스

--- algo/JS/baekjoon/2606_virus.js
+++ algo/JS/baekjoon/2606_virus.js
@@ -6,14 +6,11 @@ let m = Number(input.shift());
 let graph = [...new Array(n + 1)].map(() => []);
 let visited = new Array(n + 1).fill(false);
 let ans = 0;
-console.log('그래프', graph);
 visited[1] = true;
 
 const dfs = (start) => {
   for (const dest of graph[start]) {
-    console.log('데스트', dest);
     if (!visited[dest]) {
-      console.log('&&&&&', visited[dest]);
       visited[dest] = true;
       ans += 1;
       dfs(dest);
