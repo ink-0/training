17925e0f5c39b5f0b9a2fa42196eb4cb94945793
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-11-07 23:57:56 +0900

    [BJ] JS 2606바이러스
    - https://rrecoder.tistory.com/209

--- algo/JS/baekjoon/2606_virus.js
+++ algo/JS/baekjoon/2606_virus.js
@@ -4,26 +4,28 @@ let input = (fs.readFileSync('./test') + '').toString().trim().split('\n');
 let n = Number(input.shift());
 let m = Number(input.shift());
 let graph = [...new Array(n + 1)].map(() => []);
+// let graph2 = new Array(n + 1).fill([]);
 let visited = new Array(n + 1).fill(false);
 let ans = 0;
+
 visited[1] = true;
 
 const dfs = (start) => {
-  for (const dest of graph[start]) {
+  graph[start].map((dest) => {
     if (!visited[dest]) {
       visited[dest] = true;
       ans += 1;
       dfs(dest);
     }
-  }
+  });
 };
 
 input.map((i) => {
   const [start, dest] = i.split(' ').map((ele) => Number(ele));
   graph[start].push(dest);
   graph[dest].push(start);
-  console.log(i, 'i', graph);
 });
 
 dfs(1);
+
 console.log(ans);
