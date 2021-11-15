b0242cc5d6cc685de4ec1c273f5d55e381168db0
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-11-16 02:44:49 +0900

    [BJ]JS 1789_수들의합2

--- algo/JS/baekjoon/1789_sumNum.js
+++ algo/JS/baekjoon/1789_sumNum.js
@@ -2,22 +2,23 @@ const fs = require('fs');
 
 let input = (fs.readFileSync('./test') + '').toString().trim();
 input = Number(input);
-// let idx = 0;
-// let ans = 0;
-// while (ans < input) {
+let idx = 0;
+let ans = 0;
+// while (ans <= input) {
+//   //   console.log(ans, idx);
 //   idx += 1;
 //   ans += idx;
 // }
+// console.log(idx - 1);
 
 for (let i = 1; ans < input; i++) {
   if (ans + i <= input) {
-    sum += i;
+    ans += i;
     idx++;
+    // console.log(ans, idx);
   }
-  if (sum + i > input) {
-    console.log(cnt);
+  if (ans + i > input) {
+    console.log(idx);
     break;
   }
 }
-
-console.log(idx - 1);
