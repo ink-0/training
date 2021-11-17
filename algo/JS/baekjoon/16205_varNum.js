99dc238181577bce9b4c1a6675061c85de85d145
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-11-18 02:21:19 +0900

    [BJ] JS 16250_변수명1

--- algo/JS/baekjoon/16205_varNum.js
+++ algo/JS/baekjoon/16205_varNum.js
@@ -1,16 +1,51 @@
 const fs = require('fs');
 
-let [num, type] = (fs.readFileSync('./test') + '').toString().trim().split(' ');
+let [num, varName] = (fs.readFileSync('./test') + '')
+  .toString()
+  .trim()
+  .split(' ');
 
 let ans = [];
-ans[Number(num) - 1] = type;
+
+const getSnakeCase = (word) => {
+  let wordArr = word.split('');
+  let result = wordArr.map((word, idx) => {
+    if (idx === 0) return word.toLowerCase();
+    else if (word === word.toUpperCase() && word !== '_')
+      return '_' + word.toLowerCase();
+    else return word;
+  });
+  return result.join('');
+};
 
 const getCamelCase = (word) => {
-    word.map(w => {
-        if(word.toUpperCase())
-    })
- }
-for (let i = 0; i < 3; i++) {
-  if (!ans[i]) {
-  }
-}
+  let wordArr = word.split('');
+  let isNextUpper = false;
+  let result = wordArr.map((word, idx) => {
+    if (word === '_') {
+      isNextUpper = true;
+      return null;
+    } else if (isNextUpper) {
+      isNextUpper = false;
+      return word.toUpperCase();
+    } else if (idx === 0) return word.toLowerCase();
+    else return word;
+  });
+
+  return result.join('');
+};
+
+const getPascalCase = (word) => {
+  let wordArr = word.split('');
+  let result = wordArr.map((word, idx) => {
+    if (idx === 0) return word.toUpperCase();
+    else if (word === '_') return null;
+    else return word;
+  });
+  return result.join('');
+};
+
+// let varNameArr = varName.split('');
+console.log(getCamelCase(varName));
+console.log(getSnakeCase(varName));
+console.log(getPascalCase(varName));
