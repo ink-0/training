405348db3c53793ef76c9ad4bab36c4293f1a32d
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-11-23 00:19:29 +0900

    [LC] 64_minimumPathSum2

--- algo/JS/LeetCode/64_minimumPathSum.js
+++ algo/JS/LeetCode/64_minimumPathSum.js
@@ -1,22 +1,23 @@
 var minPathSum = function (grid) {
-  for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid[0].length; j++) {
-      if (i == 0 && j == 0) {
-        continue;
+  let mLen = grid.length;
+  let nLen = grid[0].length;
+
+  const DP = [...new Array(mLen)].map(() => []);
+
+  DP[0][0] = grid[0][0];
+
+  for (let i = 0; i < mLen; i++) {
+    for (let j = 0; j < nLen; j++) {
+      if (i === 0 && j === 0) DP[i][j] = grid[i][j];
+      // 우측 방향
+      else if (j === 0) DP[i][j] = grid[i][j] + DP[i - 1][j];
+      // 하단 방향
+      else if (i === 0) DP[i][j] = grid[i][j] + DP[i][j - 1];
+      // 대각선 방향
+      else {
+        DP[i][j] = grid[i][j] + Math.min(DP[i][j - 1], DP[i - 1][j]);
       }
-      if (i == 0) {
-        grid[i][j] = grid[i][j] + grid[i][j - 1];
-        continue;
-      }
-      if (j == 0) {
-        grid[i][j] = grid[i][j] + grid[i - 1][j];
-        continue;
-      }
-      grid[i][j] = Math.min(
-        grid[i][j] + grid[i][j - 1],
-        grid[i][j] + grid[i - 1][j]
-      );
     }
   }
-  return grid[grid.length - 1][grid[0].length - 1];
+  return DP[mLen - 1][nLen - 1];
 };
