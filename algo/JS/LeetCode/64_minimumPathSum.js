003505c2a1f63dd41cea13f8458d6cba5718ac9b
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-11-22 23:58:58 +0900

    [LC] 64_minimumPathSum

---
+++ algo/JS/LeetCode/64_minimumPathSum.js
@@ -0,0 +1,22 @@
+var minPathSum = function (grid) {
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (i == 0 && j == 0) {
+        continue;
+      }
+      if (i == 0) {
+        grid[i][j] = grid[i][j] + grid[i][j - 1];
+        continue;
+      }
+      if (j == 0) {
+        grid[i][j] = grid[i][j] + grid[i - 1][j];
+        continue;
+      }
+      grid[i][j] = Math.min(
+        grid[i][j] + grid[i][j - 1],
+        grid[i][j] + grid[i - 1][j]
+      );
+    }
+  }
+  return grid[grid.length - 1][grid[0].length - 1];
+};
