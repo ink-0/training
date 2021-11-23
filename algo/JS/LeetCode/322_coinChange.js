f18d66d09f1a5a9a5edb0048fd5c2bd55afa7c39
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-11-24 02:13:45 +0900

    [LC] 32_CoinChange2

--- algo/JS/LeetCode/322_coinChange.js
+++ algo/JS/LeetCode/322_coinChange.js
@@ -1,6 +1,6 @@
 var coinChange = function (coins, amount) {
   let DP = new Array(amount + 1).fill(amount + 1);
-  console.log(DP);
+
   let coinLen = coins.length;
 
   if (amount < 0) return -1;
@@ -8,10 +8,9 @@ var coinChange = function (coins, amount) {
   DP[0] = 0;
 
   for (let i = 1; i <= amount; i++) {
-    for (let coin of coins) {
-      if (i >= coin) {
-        DP[i] = Math.min(DP[i], DP[i - coin]) + 1;
-        // console.log(DP);
+    for (let j = 0; j < coins.length; j++) {
+      if (i >= coins[j]) {
+        DP[i] = Math.min(DP[i], DP[i - coins[j]] + 1);
       }
     }
   }
