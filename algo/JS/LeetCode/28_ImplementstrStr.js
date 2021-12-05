1c25481b2a6b7991f894d3a3e68ff93871ec6ad2
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-12-05 23:59:53 +0900

    [LC]: 28_ImplementstrStr

--- algo/JS/LeetCode/28_ImplementstrStr.js
+++
@@ -1,9 +0,0 @@
-var strStr = function (haystack, needle) {
-  let needleLen = needle.length;
-
-  for (let i = 0; i < haystack.length; i++) {
-    if (haystack[i] === needle[0]) {
-      if (haystack.substring(i, i + needleLen) === needle) return i;
-    }
-  }
-};
