28e43164f19214ab727907aa859bf33fe8e0075b
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-12-07 00:32:20 +0900

    [LC] 125_ValidPalindrome2

--- algo/JS/LeetCode/string/125_validPalindrome.js
+++ algo/JS/LeetCode/string/125_validPalindrome.js
@@ -1,10 +1,19 @@
+// var isPalindrome = function (s) {
+//   s = s.toUpperCase();
+//   let arr = [];
+//   for (let i = 0; i < s.length; i++) {
+//     if ((s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= '0' && s[i] <= '9')) {
+//       arr.push(s[i]);
+//     }
+//   }
+//   return arr.join('') === arr.reverse().join('');
+// };
+
 var isPalindrome = function (s) {
-  s = s.toUpperCase();
-  let arr = [];
-  for (let i = 0; i < s.length; i++) {
-    if ((s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= '0' && s[i] <= '9')) {
-      arr.push(s[i]);
-    }
+  const regExp = /[^\da-zA-Z]/g;
+  s = s.toUpperCase().replace(regExp, '');
+  for (let i = 0, j = s.length - 1; i <= j; i++, j--) {
+    if (s[i] !== s[j]) return false;
   }
-  return arr.join('') === arr.reverse().join('');
+  return true;
 };
