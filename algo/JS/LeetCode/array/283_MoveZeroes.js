f231e1737e0a69b376104ffd753ff5be1c42882c
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-12-10 02:44:41 +0900

    [LC] 283_MoveZeroes

---
+++ algo/JS/LeetCode/array/283_MoveZeroes.js
@@ -0,0 +1,10 @@
+var moveZeroes = function (nums) {
+  for (let i = nums.length; i >= 0; i--) {
+    if (nums[i] === 0) {
+      nums.splice(i, 1);
+      nums.push(0);
+    }
+  }
+
+  return nums;
+};
